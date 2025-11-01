93af97cca4574d2f60edf89f1e3e4278b0ecf25a
Michael <mexanoid11@mail.ru>
2025-11-01 21:10:50 +0300

    feat: implement background changer

--- src/index.js
+++ src/index.js
@@ -6,26 +6,34 @@ const ICON = {
   winter: "./assets/icons/cloud-snow.svg",
 };
 
+const BACKGROUND = {
+  summer: "url('./assets/summer-bg.jpg')",
+  rainy: "url('./assets/rainy-bg.jpg')",
+  winter: "url('./assets/winter-bg.jpg')",
+};
+
+document.body.style.backgroundImage = BACKGROUND.summer;
 const mainBox = document.querySelector("#app");
+
 const audioManager = document.createElement("div");
 audioManager.className = "audio-manager";
 mainBox.append(audioManager);
 
 const summerBox = document.createElement("div");
 summerBox.classList.add("audio-box", "summer");
-summerBox.style.backgroundImage = "url('./assets/summer-bg.jpg')";
+summerBox.style.backgroundImage = BACKGROUND.summer;
 summerBox.innerHTML = `<img src=${ICON.summer} />`;
 audioManager.appendChild(summerBox);
 
 const rainyBox = document.createElement("div");
 rainyBox.classList.add("audio-box", "rainy");
-rainyBox.style.backgroundImage = "url('./assets/rainy-bg.jpg')";
+rainyBox.style.backgroundImage = BACKGROUND.rainy;
 rainyBox.innerHTML = `<img src=${ICON.rainy} />`;
 audioManager.appendChild(rainyBox);
 
 const winterBox = document.createElement("div");
 winterBox.classList.add("audio-box", "winter");
-winterBox.style.backgroundImage = "url('./assets/winter-bg.jpg')";
+winterBox.style.backgroundImage = BACKGROUND.winter;
 winterBox.innerHTML = `<img src=${ICON.winter} />`;
 audioManager.appendChild(winterBox);
 
@@ -34,7 +42,7 @@ let currentPlayingUrl = null;
 let previousAudioBox = null;
 let previousIconLink = null;
 
-function playAudio(audioUrl, targetAudioBox, iconLink) {
+function playAudio(audioUrl, targetAudioBox, iconLink, backgroundImage) {
   if (currentPlayingUrl !== audioUrl) {
     if (activeAudio) {
       activeAudio.pause();
@@ -51,6 +59,7 @@ function playAudio(audioUrl, targetAudioBox, iconLink) {
       });
     currentPlayingUrl = audioUrl;
     targetAudioBox.innerHTML = `<img src="./assets/icons/pause.svg" />`;
+    document.body.style.backgroundImage = backgroundImage;
 
     previousAudioBox = targetAudioBox;
     previousIconLink = iconLink;
@@ -69,13 +78,28 @@ mainBox.addEventListener("click", (event) => {
   const targetAudioBox = event.target.closest(".audio-box");
   switch (targetAudioBox) {
     case summerBox:
-      playAudio("./assets/sounds/summer.mp3", targetAudioBox, ICON.summer);
+      playAudio(
+        "./assets/sounds/summer.mp3",
+        targetAudioBox,
+        ICON.summer,
+        BACKGROUND.summer
+      );
       break;
     case rainyBox:
-      playAudio("./assets/sounds/rain.mp3", targetAudioBox, ICON.rainy);
+      playAudio(
+        "./assets/sounds/rain.mp3",
+        targetAudioBox,
+        ICON.rainy,
+        BACKGROUND.rainy
+      );
       break;
     case winterBox:
-      playAudio("./assets/sounds/winter.mp3", targetAudioBox, ICON.winter);
+      playAudio(
+        "./assets/sounds/winter.mp3",
+        targetAudioBox,
+        ICON.winter,
+        BACKGROUND.winter
+      );
       break;
     default:
       break;
