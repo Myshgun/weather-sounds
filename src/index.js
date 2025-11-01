fcbf07607ba67e843c0b6a58c8e6117f99299756
Michael <mexanoid11@mail.ru>
2025-11-01 18:42:46 +0300

    feat: add basic audio manager markup and styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./index.css";
+
+const mainBox = document.querySelector("#app");
+
+const summerBox = document.createElement("div");
+summerBox.classList.add("audio-box", "summer");
+summerBox.style.backgroundImage = "url('./assets/summer-bg.jpg')";
+summerBox.innerHTML = `<img src="./assets/icons/sun.svg" />`;
+mainBox.appendChild(summerBox);
+
+const rainyBox = document.createElement("div");
+rainyBox.classList.add("audio-box", "rainy");
+rainyBox.style.backgroundImage = "url('./assets/rainy-bg.jpg')";
+rainyBox.innerHTML = `<img src="./assets/icons/cloud-rain.svg" />`;
+mainBox.appendChild(rainyBox);
+
+const winterBox = document.createElement("div");
+winterBox.classList.add("audio-box", "winter");
+winterBox.style.backgroundImage = "url('./assets/winter-bg.jpg')";
+winterBox.innerHTML = `<img src="./assets/icons/cloud-snow.svg" />`;
+mainBox.appendChild(winterBox);
